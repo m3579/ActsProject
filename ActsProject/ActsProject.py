9683980b524ec6af9a4a01bf1bf5107c693d26a2
M3579 <mihirkasmalkar@gmail.com>
2015-09-08 18:06:31 -0700

    Coded the animation framework

---
+++ ActsProject/ActsProject.py
@@ -0,0 +1,37 @@
+from art import Terminal
+
+terminal = Terminal()
+
+terminal.resetdelay = 1
+
+terminal.draw(
+    """
+
+    *
+
+    """
+    )
+
+terminal.draw(
+    """
+
+     *
+
+    """
+    )
+
+terminal.draw(
+    """
+
+      *
+
+    """
+    )
+
+terminal.draw(
+    """
+
+       *
+
+    """
+    )
---
+++ ActsProject/art.py
@@ -0,0 +1,47 @@
+from time import sleep
+import colorama
+import sys
+
+class Terminal():
+    def __init__(self, maxRows=50, maxColumns=79):
+        colorama.init()
+
+        self.maxRows = maxRows
+        self.maxColumns = maxColumns
+
+        self.frame = 0
+
+        self.resetdelay = 0.02
+
+    def draw(self, text, resetdelay=None):
+        if resetdelay == None:
+            resetdelay = self.resetdelay
+
+        lines = text.split("\n")
+
+        lineCount = 0
+
+        for line in lines:
+            length = len(line)
+
+            if length > self.maxColumns:
+                error(lineCount + 1, "Too many columns in this animation")
+            elif length < self.maxColumns:
+                while len(line) < self.maxColumns:
+                    line += " "
+
+            print(line + "\n")
+
+            lineCount += 1
+
+            self.reset(lineCount, resetdelay)
+
+    def reset(self, lineCount, delay):
+        sleep(delay)
+
+        for i in range(lineCount):
+            sys.stdout.write("\033[1A\r")
+
+    def error(self, line, message):
+        print("Error (", self.frame, " : ", line, "): ", message, sep="")
+        sys.exit(1)
